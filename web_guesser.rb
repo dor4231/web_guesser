d4a52db09991e24a3d00d759c0bc0b1342605673
dor4231 <dor4231@gmail.com>
2017-09-13 02:39:34 +0300

    I3: Gussing game complate!

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number = rand(101)
+SECRET_NUMBER = rand(101)
+massage = ""
 
 get "/" do
-    erb :index, :locals => {:number => secret_number}
+    erb :index, :locals => {:number => SECRET_NUMBER, :massage => massage}
+    massage = check_guess(params["guess"].to_i)
+    erb :index, :locals => {:number => SECRET_NUMBER, :massage => massage}
+end
+
+
+def check_guess (number)
+    if params["guess"].to_i - SECRET_NUMBER.to_i > 5
+        return "Your guess is WEY TOO HIGH!"
+    elsif params["guess"].to_i - SECRET_NUMBER.to_i < -5
+        return "Your guess is WEY TOO LOW!"
+    elsif params["guess"].to_i > SECRET_NUMBER.to_i
+        return "Your guess is TOO HIGH!"
+    elsif params["guess"].to_i < SECRET_NUMBER.to_i
+        return "Your guess is TOO LOW!"
+    else
+        return "You are CORRECT!"
+    end
+
 end
