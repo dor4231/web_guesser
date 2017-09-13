19e1493b0b005c07e1f572c58ed34ed74ba83d0b
dor4231 <dor4231@gmail.com>
2017-09-13 04:15:54 +0300

    Compleat the game!

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,27 +1,61 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(101)
-massage = ""
+$secret_number = rand(101)
+$massage = "First"
+$color = "darkgray"
+@@life = 6
+$cheat = "none"
 
 get "/" do
-    erb :index, :locals => {:number => SECRET_NUMBER, :massage => massage}
-    massage = check_guess(params["guess"].to_i)
-    erb :index, :locals => {:number => SECRET_NUMBER, :massage => massage}
+    guess = params["guess"]
+    if params["cheat"] == "true"
+        $cheat = "block"
+    else
+        $cheat = "none"
+    end
+    $massage = check_guess(guess)
+    erb :index, :locals => {:number => $secret_number, 
+                            :massage => $massage,
+                            :color => $color,
+                            :cheat => $cheat}
 end
 
 
 def check_guess (number)
-    if params["guess"].to_i - SECRET_NUMBER.to_i > 5
-        return "Your guess is WEY TOO HIGH!"
-    elsif params["guess"].to_i - SECRET_NUMBER.to_i < -5
-        return "Your guess is WEY TOO LOW!"
-    elsif params["guess"].to_i > SECRET_NUMBER.to_i
-        return "Your guess is TOO HIGH!"
-    elsif params["guess"].to_i < SECRET_NUMBER.to_i
-        return "Your guess is TOO LOW!"
+    if number == ""
+        $color = "darkgray"
+        return "First"
+    elsif params["guess"].to_i - $secret_number.to_i > 5
+        add = life_check()
+        return "Your guess is WEY TOO HIGH! " + add
+    elsif params["guess"].to_i - $secret_number.to_i < -5
+        add = life_check()
+        return "Your guess is WEY TOO LOW! " + add
+    elsif params["guess"].to_i > $secret_number.to_i
+        $color = "lightblue"
+        add = life_check()
+        return "Your guess is TOO HIGH! " + add
+    elsif params["guess"].to_i < $secret_number.to_i
+        $color = "lightblue"
+        add = life_check()
+        return "Your guess is TOO LOW! " + add
     else
-        return "You are CORRECT!"
+        $color = "green"
+        $secret_number = rand(101)
+        @@life = 6
+        return "You are CORRECT! Press the 'Guess!' botton to generate another number."
     end
+end
 
+def life_check ()
+    @@life -= 1
+    if @@life > 0
+        return @@life.to_s + " guess left!"
+    else
+        $secret_number = rand(101)
+        @@life = 6
+        $color = "darkgray"
+        return "No more guess left! Generating another number..."
+    end
 end
